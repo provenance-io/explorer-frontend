d10a6f3a00412647a63d9f0e1505dac02dad1483
VigM <vmikulis@figure.com>
2021-03-31 14:14:44 -0600

    manually add in fonts, no need to download them.  Update nginx config file and docker prod files.  Update global styles to pull fonts locally. Create new docker:prod and docker:local commands to spin app up in a docker container (awesome for testing nginx config stuff)

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -1,9 +1,34 @@
 import { createGlobalStyle } from 'styled-components';
 import styledNormalize from 'styled-normalize';
+import montserratRegular from './Font/montserrat/regular.ttf';
+import montserratMedium from './Font/montserrat/medium.ttf';
+import montserratBold from './Font/montserrat/bold.ttf';
 
 export const GlobalStyle = createGlobalStyle`
   ${styledNormalize}
-
+  
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 400;
+    src: url(${montserratRegular}) format('truetype');
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 500;
+    src: url(${montserratMedium}) format('truetype');
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 700;
+    src: url(${montserratBold}) format('truetype');
+    font-display: swap;
+  }
+ 
   html {
     font-size: 62.5%; /* =10px and will allow for all rem sizing to be easier, 1.4 rem => 14px*/
   }
