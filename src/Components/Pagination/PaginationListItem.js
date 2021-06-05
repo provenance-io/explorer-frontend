14dfb8987d192cddd4c71866eba671a9c1da3b1a
Milos <milosmilic032@gmail.com>
2021-06-05 18:46:30 +0200

    fixed pegination link bug

--- src/Components/Pagination/PaginationListItem.js
+++ src/Components/Pagination/PaginationListItem.js
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 
-const PaginatorListItem = styled.li.attrs(({ className = 'paginator-list-item' }) => ({ className }))`
-  width: ${({ secondary }) => (secondary ? '19px' : '34px')};
+const PaginatorListItem = styled.li.attrs(({ className = 'paginator-list-item' }) => ({
+  className,
+}))`
+  width: auto;
   height: ${({ secondary }) => (secondary ? '19px' : '34px')};
   display: flex;
   align-items: center;
@@ -9,12 +11,14 @@ const PaginatorListItem = styled.li.attrs(({ className = 'paginator-list-item' }
   border-width: ${({ secondary, active }) => (secondary ? (active ? '0 0 1px 0' : '0') : '2px')};
   border-style: solid;
   border-color: ${({ theme }) => theme.BORDER_THEME};
-  background-color: ${({ secondary, theme, active }) => (!secondary && active ? theme.BACKGROUND_THEME : 'inherit')};
+  background-color: ${({ secondary, theme, active }) =>
+    !secondary && active ? theme.BACKGROUND_THEME : 'inherit'};
   > button {
     color: ${({ theme }) => theme.FONT_PRIMARY};
   }
   + li {
-    margin-left: -2px;
+    margin-left: 3.5px;
+    margin-right: 3.5px;
   }
 `;
 
