7115b133475a922ab839f7f5fc5afed22eca92a7
AJ Webb <webbushka@gmail.com>
2022-05-20 09:22:45 -1000

    fix the orderbook calls

--- src/redux/features/orderbook/orderbookSlice.ts
+++ src/redux/features/orderbook/orderbookSlice.ts
@@ -1,7 +1,7 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { RootState } from "redux/app/store";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { RootState } from 'redux/app/store';
 import { ORDER_BOOK_URL, ORDER_BOOK_VOLUME_URL } from 'consts';
-import { ajax } from "../api";
+import { ajax } from '../api';
 
 interface DailyPrice {
   ask: number;
@@ -14,7 +14,7 @@ interface DailyPrice {
   target_currency: string;
   target_volume: number;
   ticker_id: string;
-};
+}
 
 export interface PriceHistory {
   base_volume: number;
@@ -23,7 +23,7 @@ export interface PriceHistory {
   trade_id: number;
   trade_timestamp: string | number; // Comes as epoch number, converted to string
   type: string;
-};
+}
 
 interface DailyVolume {
   contractAddress: string;
@@ -35,38 +35,38 @@ interface DailyVolume {
   lowDisplayPricePerDisplayUnit: string;
   latestPricePerUnit: string;
   latestDisplayPricePerDisplayUnit: string;
-};
+}
 
 interface OrderbookState {
-  dailyPrice: DailyPrice,
+  dailyPrice: DailyPrice;
   dailyPriceFailed: boolean;
   dailyPriceLoading: boolean;
-  priceHistory: PriceHistory[],
+  priceHistory: PriceHistory[];
   priceHistoryFailed: boolean;
   priceHistoryLoading: boolean;
-  dailyVolume: DailyVolume["displayVolumeTraded"];
+  dailyVolume: DailyVolume['displayVolumeTraded'];
   dailyVolumeLoading: boolean;
-};
+}
 
 export const initialState: OrderbookState = {
   dailyPrice: {
     ask: 0,
-    base_currency: "",
+    base_currency: '',
     base_volume: 0,
     bid: 0,
     high: 0,
     last_price: 0,
     low: 0,
-    target_currency: "",
+    target_currency: '',
     target_volume: 0,
-    ticker_id: "",
+    ticker_id: '',
   },
   dailyPriceFailed: false,
   dailyPriceLoading: false,
   priceHistory: [],
   priceHistoryFailed: false,
   priceHistoryLoading: false,
-  dailyVolume: "",
+  dailyVolume: '',
   dailyVolumeLoading: false,
 };
 
@@ -80,34 +80,35 @@ export const GET_DAILY_VOLUME = 'ORDERBOOK::GET_DAILY_VOLUME';
 /* -----------------
 ** ACTIONS
 -------------------*/
-export const getDailyPrice = createAsyncThunk(
-  GET_DAILY_PRICE,
-  () =>
-    ajax({
-      url: `${ORDER_BOOK_URL}/tickers`,
-    })
+export const getDailyPrice = createAsyncThunk(GET_DAILY_PRICE, () =>
+  ajax({
+    config: {
+      withCredentials: false,
+    },
+    url: `${ORDER_BOOK_URL}/tickers`,
+  })
 );
 
 export const getPriceHistory = createAsyncThunk(
   GET_PRICE_HISTORY,
-  ({
-    start_time = '', 
-    end_time = '',
-  } : {
-    start_time: string,
-    end_time: string,
-  }) =>
+  ({ start_time = '', end_time = '' }: { start_time: string; end_time: string }) =>
     ajax({
-      url: `${ORDER_BOOK_URL}/historical_trades?ticker_id=HASH_USD&type=buy${start_time && `&start_time=${start_time}`}${end_time && `&end_time=${end_time}`}`,
+      config: {
+        withCredentials: false,
+      },
+      url: `${ORDER_BOOK_URL}/historical_trades?ticker_id=HASH_USD&type=buy${
+        start_time && `&start_time=${start_time}`
+      }${end_time && `&end_time=${end_time}`}`,
     })
 );
 
-export const getDailyVolume = createAsyncThunk(
-  GET_DAILY_VOLUME,
-  () =>
-    ajax({
-      url: ORDER_BOOK_VOLUME_URL,
-    })
+export const getDailyVolume = createAsyncThunk(GET_DAILY_VOLUME, () =>
+  ajax({
+    config: {
+      withCredentials: false,
+    },
+    url: ORDER_BOOK_VOLUME_URL,
+  })
 );
 
 export const orderbookActions = {
@@ -124,55 +125,57 @@ export const orderbookSlice = createSlice({
   reducers: {},
   extraReducers(builder) {
     builder
-    /* -----------------
+      /* -----------------
     GET_DAILY_PRICE
     -------------------*/
-    .addCase(getDailyPrice.pending, (state) => {
-      state.dailyPriceLoading = true;
-      state.dailyPriceFailed = false;
-    })
-    .addCase(getDailyPrice.fulfilled, (state, { payload }) => {
-      state.dailyPriceLoading = false;
-      state.dailyPriceFailed = false;
-      state.dailyPrice = payload.data.find((ticker: DailyPrice) => ticker.ticker_id === 'HASH_USD');
-    })
-    .addCase(getDailyPrice.rejected, (state) => {
-      state.dailyPriceLoading = false;
-      state.dailyPriceFailed = true;
-    })
-    /* -----------------
+      .addCase(getDailyPrice.pending, (state) => {
+        state.dailyPriceLoading = true;
+        state.dailyPriceFailed = false;
+      })
+      .addCase(getDailyPrice.fulfilled, (state, { payload }) => {
+        state.dailyPriceLoading = false;
+        state.dailyPriceFailed = false;
+        state.dailyPrice = payload.data.find(
+          (ticker: DailyPrice) => ticker.ticker_id === 'HASH_USD'
+        );
+      })
+      .addCase(getDailyPrice.rejected, (state) => {
+        state.dailyPriceLoading = false;
+        state.dailyPriceFailed = true;
+      })
+      /* -----------------
     GET_PRICE_HISTORY
     -------------------*/
-    .addCase(getPriceHistory.pending, (state) => {
-      state.priceHistoryLoading = true;
-      state.priceHistoryFailed = false;
-    })
-    .addCase(getPriceHistory.fulfilled, (state, { payload }) => {
-      state.priceHistoryLoading = false;
-      state.priceHistoryFailed = false;
-      state.priceHistory = payload.data.buy.map((item: PriceHistory) => {
-        // Convert epoch seconds to ISO time format
-        item.trade_timestamp = new Date((item.trade_timestamp as number) * 1000).toISOString();
-        return { ...item };
-      });
-    })
-    .addCase(getPriceHistory.rejected, (state) => {
-      state.priceHistoryLoading = false;
-      state.priceHistoryFailed = true;
-    })
-    /* -----------------
+      .addCase(getPriceHistory.pending, (state) => {
+        state.priceHistoryLoading = true;
+        state.priceHistoryFailed = false;
+      })
+      .addCase(getPriceHistory.fulfilled, (state, { payload }) => {
+        state.priceHistoryLoading = false;
+        state.priceHistoryFailed = false;
+        state.priceHistory = payload.data.buy.map((item: PriceHistory) => {
+          // Convert epoch seconds to ISO time format
+          item.trade_timestamp = new Date((item.trade_timestamp as number) * 1000).toISOString();
+          return { ...item };
+        });
+      })
+      .addCase(getPriceHistory.rejected, (state) => {
+        state.priceHistoryLoading = false;
+        state.priceHistoryFailed = true;
+      })
+      /* -----------------
     GET_DAILY_VOLUME
     -------------------*/
-    .addCase(getDailyVolume.pending, (state) => {
-      state.dailyVolumeLoading = true;
-    })
-    .addCase(getDailyVolume.fulfilled, (state, { payload }) => {
-      state.dailyVolumeLoading = false;
-      state.dailyVolume = payload.data.displayVolumeTraded;
-    })
-    .addCase(getDailyVolume.rejected, (state) => {
-      state.dailyVolumeLoading = false;
-    });
+      .addCase(getDailyVolume.pending, (state) => {
+        state.dailyVolumeLoading = true;
+      })
+      .addCase(getDailyVolume.fulfilled, (state, { payload }) => {
+        state.dailyVolumeLoading = false;
+        state.dailyVolume = payload.data.displayVolumeTraded;
+      })
+      .addCase(getDailyVolume.rejected, (state) => {
+        state.dailyVolumeLoading = false;
+      });
   },
 });
 
