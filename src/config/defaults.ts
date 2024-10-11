ef30adb21a28f3cc3b9cfe67cb1621a2574b2f9d
Jarryd Allison <jallison@figure.com>
2024-10-11 14:53:51 -0600

    feat: Add mainnet

--- src/config/defaults.ts
+++ src/config/defaults.ts
@@ -1,5 +1,7 @@
-import { LocalChainAssets, LocalChainConfig } from "./localprovenance";
-import { TestnetChainAssets, TestnetChainConfig } from "./testnetprovenance";
+import { isProd } from '../consts';
+// import { LocalChainAssets, LocalChainConfig } from "./localprovenance";
+import { TestnetChainAssets, TestnetChainConfig } from './testnetprovenance';
+import { MainnetChainAssets } from './mainnetprovenance';
 
 export const MAIN_CHAIN_NAME = 'provenance';
 export const MAIN_RPC_ENDPOINT = 'https://rpc.provenance.io';
@@ -10,7 +12,7 @@ export const LOCAL_RPC_ENDPOINT = 'http://localhost:26657';
 export const TESTNET_CHAIN_NAME = 'provenancetestnet';
 export const TESTNET_RPC_ENDPOINT = 'https://rpc.test.provenance.io:443';
 
-export const CHAIN_NAME = TESTNET_CHAIN_NAME;
-export const RPC_ENDPOINT = TESTNET_RPC_ENDPOINT;
-export const CHAIN_CONFIG = TestnetChainConfig;
-export const CHAIN_ASSETS = TestnetChainAssets;
+export const CHAIN_NAME = isProd ? MAIN_CHAIN_NAME : TESTNET_CHAIN_NAME;
+export const RPC_ENDPOINT = isProd ? MAIN_RPC_ENDPOINT : TESTNET_RPC_ENDPOINT;
+export const CHAIN_CONFIG = isProd ? MainnetChainAssets : TestnetChainConfig;
+export const CHAIN_ASSETS = isProd ? MainnetChainAssets : TestnetChainAssets;
