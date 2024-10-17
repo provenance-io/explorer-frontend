e0756dd0931d561b8586a4ee681601b2bf976447
Jarryd Allison <jallison@figure.com>
2024-10-17 11:34:32 -0600

    feat: Remove walletConnectOptions

--- src/Providers/CosmosProvider.tsx
+++ src/Providers/CosmosProvider.tsx
@@ -89,18 +89,18 @@ export const CosmosProvider = ({ children }: { children: React.ReactNode }) => {
             },
           },
         }}
-        walletConnectOptions={{
-          signClient: {
-            projectId: 'a8510432ebb71e6948cfd6cde54b70f7',
-            relayUrl: 'wss://relay.walletconnect.org',
-            metadata: {
-              name: 'CosmosKit Template',
-              description: 'CosmosKit dapp template',
-              url: 'https://docs.cosmology.zone/cosmos-kit/',
-              icons: [],
-            },
-          },
-        }}
+        // walletConnectOptions={{
+        //   signClient: {
+        //     projectId: 'a8510432ebb71e6948cfd6cde54b70f7',
+        //     relayUrl: 'wss://relay.walletconnect.org',
+        //     metadata: {
+        //       name: 'CosmosKit Template',
+        //       description: 'CosmosKit dapp template',
+        //       url: 'https://docs.cosmology.zone/cosmos-kit/',
+        //       icons: [],
+        //     },
+        //   },
+        // }}
         // @ts-ignore
         signerOptions={signerOptions}
       >
