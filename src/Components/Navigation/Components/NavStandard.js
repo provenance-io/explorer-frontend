ce67b5b0c529ec702e79a8544bd437491b322dfa
jarrydallison <jarryd.allison@gmail.com>
2025-08-01 14:58:16 -0600

    feat: add link to Pulse, update logos

--- src/Components/Navigation/Components/NavStandard.js
+++ src/Components/Navigation/Components/NavStandard.js
@@ -10,6 +10,7 @@ import Toggle from '../../Toggle';
 import UserAccount from '../../UserAccount';
 import AnnouncementMenu from '../../AnnouncementMenu';
 import NavHeaders from './NavHeaders';
+import Button from '../../Button';
 
 const NavigationWrapper = styled.div`
   display: flex;
@@ -23,6 +24,34 @@ const NavigationWrapper = styled.div`
   background: ${({ theme }) => theme.BACKGROUND_NAV};
 `;
 
+const ExternalLink = styled.a`
+  align-items: center;
+  color: ${({ theme, color }) => theme[`BUTTON_${color}_FONT`]};
+  background: ${({ theme, color }) => theme[`BUTTON_${color}`]};
+  border-radius: 4px;
+  border: 1px solid ${({ theme, color }) => theme[`BUTTON_${color}`]};
+  padding: 6px 12px;
+  font-size: 0.875rem;
+  cursor: pointer;
+  &:focus {
+    background: ${({ disabled, theme, color }) => !disabled && theme[`BUTTON_${color}_FOCUS`]};
+    border-color: ${({ theme, color }) => theme[`BUTTON_${color}_FOCUS`]};
+  }
+  &:hover {
+    background: ${({ disabled, theme, color }) => !disabled && theme[`BUTTON_${color}_HOVER`]};
+    border-color: ${({ theme, color }) => theme[`BUTTON_${color}_FOCUS`]};
+  }
+  &:active {
+    background: ${({ disabled, theme, color }) => !disabled && theme[`BUTTON_${color}_ACTIVE`]};
+    border-color: ${({ theme, color }) => theme[`BUTTON_${color}_FOCUS`]};
+  }
+  &:disabled {
+    background: ${({ theme }) => theme.BUTTON_DISABLED};
+    cursor: not-allowed;
+    border-color: ${({ theme }) => theme.BUTTON_DISABLED};
+  }
+`;
+
 const NavStandard = () => {
   const { setTheme } = useApp();
   const { themeName } = useColorScheme();
@@ -34,6 +63,14 @@ const NavStandard = () => {
         <Link to={Path.HOME_URL} title="Provenance Blockchain Explorer | Home">
           <Sprite icon="LOGO" height="32px" />
         </Link>
+        <ExternalLink
+          href="https://www.provenance.io/pulse"
+          target="_blank"
+          rel="noopener noreferrer"
+          color="primary"
+        >
+          Pulse
+        </ExternalLink>
         <SearchBar />
         <AnnouncementMenu />
         <UserAccount />
